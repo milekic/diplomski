701f6c0083da0ba1a9eb75958d1bb0e8255a2ab2
Ognjen <milekic.ognjen@gmail.com>
2026-02-18 08:43:23 +0100

    generisanje dogadjaja na backendu

---
+++ gis-backend/DTOs/Realtime/MeasurementRealtimeDto.cs
@@ -0,0 +1,15 @@
+﻿namespace gis_backend.DTOs.Realtime
+{
+    public class MeasurementRealtimeDto
+    {
+        public int AreaMonitorId { get; set; }
+        public int AreaId { get; set; }
+        public int EventTypeId { get; set; }
+
+        public double Value { get; set; }
+        public DateTime MeasuredAtUtc { get; set; }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
